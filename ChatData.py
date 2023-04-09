14458fe31ed71cd24c001a00d7434f5e10c37e42
VINUK <58259367+VINUK0@users.noreply.github.com>
2023-04-09 16:15:19 +0530

    Updated Chatdata.py To Work With Tab_Sp

--- ChatData.py
+++ ChatData.py
@@ -1,41 +1,39 @@
 from torch.utils.data import Dataset
-import json
+from Setting import SETTINGS
+import re
+def preprocess(s):
+    s = s.replace('\n', '')
+    s = s.replace('...', '.')
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = s.replace('\'', '')
+    s = s.replace(',', '')
+    s = s.replace('-', ' ')
+    s = re.sub(' +', ' ', s)
+    return s
+
 
 class ChatData(Dataset):
     def __init__(self, path:str, tokenizer):
-        self.data = json.load(open(path, "r"))
+        pairs = []
+        with open(SETTINGS['Dataset_path'], 'r') as f:
+            for line in f:
+                pair = line.strip().split('\t')
+                pairs.append([pair[0], pair[1]])
 
-#        self.X = []
-#        for i in self.data:
-#            for j in i:
-#                self.X.append(j)
+        #preprocess pairs
+        for pair in pairs:
+            pair[0] = preprocess(pair[0].lower())
+            pair[1] = preprocess(pair[1].lower())
 
-#        for idx, i in enumerate(self.X):
-#            try:
-#                self.X[idx] = "<sos> " + i + self.X[idx+1] + " <eos>"
-#            except:
-#                break        
-        
-        self.X = []
-        for i in self.data:
-             for j in i['dialog']:
-                 self.X.append(j['text'])
+        self.input_data = ['<sos> ' + pair[0] + ' <eos> ' + '<sos> ' + pair[1] + ' <eos>' for pair in pairs]
+        self.input_data = self.input_data[:5000]
 
-        for idx, i in enumerate(self.X):
-            try:
-                self.X[idx] = "<sos> " + i + self.X[idx+1] + " <eos>"
-            except:
-                break
-
-        self.X = self.X[:5000]
-        
-
-        self.X_encoded = tokenizer(self.X,max_length=204, truncation=True, padding="max_length", return_tensors="pt")
+        self.X_encoded = tokenizer(self.input_data, max_length=208, truncation=True, padding="max_length", return_tensors="pt")
         self.input_ids = self.X_encoded['input_ids']
         self.attention_mask = self.X_encoded['attention_mask']
 
     def __len__(self):
-        return len(self.X)
+        return len(self.input_data)
 
     def __getitem__(self, idx):
         return (self.input_ids[idx], self.attention_mask[idx])
