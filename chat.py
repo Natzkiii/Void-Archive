f16d4aa68eaafcc2f1df6ff11f417d4ff2349db3
VINUK <58259367+VINUK0@users.noreply.github.com>
2023-04-09 20:58:33 +0530

    Added Advanced Chathistory

--- chat.py
+++ chat.py
@@ -6,6 +6,7 @@ from Setting import SETTINGS, Seed
 from torch.utils.data import DataLoader
 import tqdm
 import torch
+import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu" #"mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Device Used To Inference {device}")
@@ -19,6 +20,8 @@ model = GPT2LMHeadModel.from_pretrained(SETTINGS['raw_model'])
 model.resize_token_embeddings(len(tokenizer))
 
 model = model.to(device)
+np.random.seed(42)
+randseed = np.random.randint(10000)
 
 def load(model, optim):
     model.load_state_dict(torch.load(SETTINGS['inference_model_path']))
@@ -28,21 +31,26 @@ chat_history = []
 
 def infer(inp, chat_history):
     # Encode the input text
+    torch.manual_seed(Seed['infer_seed'])
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(Seed['infer_seed'])
+
     
     input_text = " ".join(chat_history[-1000:]) # limit chat history to last 1000 messages
-    inp = tokenizer(input_text + inp, return_tensors="pt")
+    inp = tokenizer(input_text + user, return_tensors="pt")
     X = inp["input_ids"].to(device)
     a = inp["attention_mask"].to(device)
-    
+    print(input_text)
     # Generate the output text
-    output = model.generate(X, attention_mask=a, max_new_tokens=200, min_new_tokens=10, do_sample=True, num_beams=4, temperature=0.7, pad_token_id=tokenizer.eos_token_id, early_stopping=True)
+    # change temperature, num_beams to get better results
+    output = model.generate(X, attention_mask=a, max_new_tokens=80, min_new_tokens=10, do_sample=True, num_beams=2, temperature=1.0, pad_token_id=tokenizer.eos_token_id, early_stopping=True)
     output = tokenizer.decode(output[0], skip_special_tokens=True)
     
     # Split the input text and generated text, and return only the generated text
     input_tokens = tokenizer.encode(inp["input_ids"].tolist()[0], skip_special_tokens=True)
     generated_tokens = tokenizer.encode(output)
     generated_text = tokenizer.decode(generated_tokens[len(input_tokens):], skip_special_tokens=True)
-    
+    chat_history.append(generated_text)
     return generated_text
 
 #optim = AdamW(model.parameters(), lr=1e-3, weight_decay=0.0)
