feb487d12c9e6db7b84123748b2cc48020477d7e
VINUK <58259367+VINUK0@users.noreply.github.com>
2023-04-09 20:52:39 +0530

    Fixed Loading Checkpoint Begin Messed Up
    My Friend Pasindu Messed Up By Loading A Saved Model To Train By Calling model.eval() and i fixed that

--- train.py
+++ train.py
@@ -11,12 +11,12 @@ from Setting import SETTINGS, Seed
 device = "cuda" if torch.cuda.is_available() else "cpu" #"mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Device Using To Train {device}")
 
-print("Bing", end="\r")
+
 tokenizer = GPT2Tokenizer.from_pretrained(SETTINGS['raw_model'])
 tokenizer.add_special_tokens({"pad_token": "<pad>", 
                                 "bos_token": "<sos>",
                                 "eos_token": "<eos>"})
-print("Bang", end="\r")
+
 model = GPT2LMHeadModel.from_pretrained(SETTINGS['raw_model'])
 model.resize_token_embeddings(len(tokenizer))
 model = model.to(device)
@@ -24,6 +24,19 @@ chatData = ChatData(SETTINGS['Dataset_path'], tokenizer)
 chatData =  DataLoader(chatData, SETTINGS['batch_size'])
 model_path = SETTINGS['model_path']
 epochs = SETTINGS['epochs']
+#optim = AdamW(model.parameters())
+optim = Adafactor(
+    model.parameters(),
+    lr=1e-3,
+    eps=(1e-30, 1e-3),
+    clip_threshold=1.0,
+    decay_rate=-0.8,
+    beta1=None,
+    weight_decay=0.0,
+    relative_step=False,
+    scale_parameter=False,
+    warmup_init=False,
+)
 print("Training..")
 
 start_time = time.time()
@@ -33,12 +46,17 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed_all(Seed["train_seed"])
 
 
-
-def train(chatData, model, optim):
+def load(model, optim):
     print("Loading Checkpoint")
-    if(os.path.exists(model_path)):
-        model.load_state_dict(torch.load(model_path))
-        model.eval()
+if(os.path.exists(SETTINGS['model_path'])):
+        model.load_state_dict(torch.load(SETTINGS['model_path']))
+        model.train()
+    
+def train(chatData, model, optim):
+#    print("Loading Checkpoint")
+#    if(os.path.exists(model_path)):
+#        model.load_state_dict(torch.load(model_path))
+#        model.eval()
     loss_values = []
     last_save = 0
     try:
@@ -46,6 +64,7 @@ def train(chatData, model, optim):
           position=0, desc="epochs", colour="green") as progBar1, tqdm.tqdm(range(len(chatData)),
             position=1, desc="training", colour="yellow") as progBar0:
             for i in range(epochs):
+                model.train()
                 epoch_loss = 0.0
                 for X, a in chatData:
                     X = X.to(device)
@@ -80,25 +99,8 @@ def train(chatData, model, optim):
         print("Saved To " +  SETTINGS['saved_model_path'])
         print('Time Spend Training ', str(x), ' Seconds')
         exit(0)
-
-model.train()
-
-#optim = AdamW(model.parameters())
-optim = Adafactor(
-    model.parameters(),
-    lr=1e-3,
-    eps=(1e-30, 1e-3),
-    clip_threshold=1.0,
-    decay_rate=-0.8,
-    beta1=None,
-    weight_decay=0.0,
-    relative_step=False,
-    scale_parameter=False,
-    warmup_init=False,
-)
-
+#print("saving the model")
+#torch.save(model.state_dict(), SETTINGS['saved_model_path'])
+#print("Done")
 
 train(chatData, model, optim)
-print("saving the model")
-torch.save(model.state_dict(), SETTINGS['saved_model_path'])
-print("Done")
