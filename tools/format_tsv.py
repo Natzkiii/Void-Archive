ef5559e718b0ff2976c542ecbd75c32d3f1812bc
psw01 <57420099+psw01@users.noreply.github.com>
2023-04-11 14:31:38 +0530

    Update format_tsv.py

--- tools/format_tsv.py
+++ tools/format_tsv.py
@@ -1,23 +1,26 @@
 # array converter
-
-dataPath = '/notebooks/Inferno_Hex/data/'
-
 from datasets import load_dataset
-import json, sys
+import json, sys, os
 
 dataset = load_dataset("daily_dialog")
 
 max_lines = 100
 min_len = 5
-
+dataPath = '/notebooks/Inferno_Hex/data/'
+file_name="data.tsv"
 
 lines = 0
-with open(dataPath + 'data.tsv', "a") as f:
-    toWrite = []
+
+os.remove(dataPath + file_name)
+with open(dataPath + file_name, "a") as f:
+    f.write("p1 \t p2\n")
     for dialogs in dataset['test']:
         for i in range(0, len(dialogs["dialog"]), 2):
             try:
+                if("\t" in dialogs["dialog"][i] or "\t" in dialogs["dialog"][i+1]):
+                    print('tab found!!!')
                 pair = dialogs["dialog"][i].strip() + "\t" + dialogs["dialog"][i + 1].strip() + "\n"
+                pair = f'{dialogs["dialog"][i].strip()}\t{dialogs["dialog"][i + 1].strip()}\n'
                 # print(i, pair)
                 f.write(pair)
                 lines += 1
