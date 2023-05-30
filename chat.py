4c8e805dbd6d67fee5eb372861e0f1b4510574da
VINUK <58259367+VINUK0@users.noreply.github.com>
2023-05-30 15:17:04 +0530

    chat.py version 2.0
    We Are Now Running Pytorch 2.0
    2x Faster Response Time
    Low Memory Usage
    More Stability
    Overall Faster Code

--- chat.py
+++ chat.py
@@ -1,35 +1,49 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, set_seed
 from transformers.optimization import Adafactor, AdafactorSchedule
 from ChatData import ChatData
+from colorama import Fore
 from torch.optim import AdamW
 from Setting import SETTINGS, Seed
 from torch.utils.data import DataLoader
 import tqdm
+import os
 import torch
 import numpy as np
 import re
+import logging
+logging.getLogger("transformers.modeling_utils").setLevel(logging.ERROR)
 
-config_path ="m/config.json"
-config = GPT2Config.from_json_file(config_path)
+config_path ="config/config.json"
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu" #"mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Device Used To Inference {device}")
+print(f"{Fore.GREEN}Device Being Use To Inference {Fore.LIGHTRED_EX}{device}")
 
 tokenizer = GPT2Tokenizer.from_pretrained('tokenizer/')
 tokenizer.add_special_tokens({"pad_token": "<pad>", 
+                              "sep_token": "<sep>", 
                                 "bos_token": "<sos>",
                                 "eos_token": "<eos>"})
 
-model = GPT2LMHeadModel.from_pretrained(SETTINGS['raw_model'])
+model = GPT2LMHeadModel.from_pretrained(SETTINGS['raw_model'], ignore_mismatched_sizes=True, config=config_path)
+# model = torch.compile(model, mode="max-autotune")
 model.resize_token_embeddings(len(tokenizer))
-
+model = torch.compile(model)
 model = model.to(device)
 
-def load(model, optim):
-    model.load_state_dict(torch.load(SETTINGS['inference_model_path']))
-    model.eval()
-    
+
+def load(model):
+    print("Loading Checkpoint")
+    if os.path.exists(SETTINGS['inference_model_path']):
+        checkpoint = torch.load(SETTINGS['inference_model_path'], map_location=torch.device('cpu'))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.eval()
+        last_save = checkpoint['last_save']
+        print(f"{Fore.GREEN}Model Is Trained For Toatal Of {Fore.LIGHTRED_EX}{last_save}")
+load(model)
+        
 chat_history = []
+
 
 def infer(inp, chat_history):
     # Encode the input text
@@ -38,18 +52,18 @@ def infer(inp, chat_history):
     torch.manual_seed(Seed['infer_seed'])
     if torch.cuda.is_available():
      torch.cuda.manual_seed_all(Seed['infer_seed'])
-
     
-    input_text = ", ".join(chat_history) # limit chat history to last 1000 messages
-    while len(" ".join(chat_history)) > 80:
-        chat_history.pop(0)
+    max_chars = 400 # maximum number of characters to keep
+    chat_history_str = "".join(chat_history) # join the chat history into a string
+    input_text = chat_history_str[-max_chars:] # keep only the first `max_chars` characters
+
     inp = tokenizer(input_text + user.lower(), return_tensors="pt")
     X = inp["input_ids"].to(device)
     a = inp["attention_mask"].to(device)
     print(input_text)
     # Generate the output text
     # change temperature, num_beams to get better results
-    output = model.generate(X, attention_mask=a, max_new_tokens=50, min_new_tokens=1, do_sample=True, num_beams=1, temperature=1.0, repetition_penalty=1.0, pad_token_id=tokenizer.eos_token_id, early_stopping=True)
+    output = model.generate(X, attention_mask=a, max_new_tokens=100, min_new_tokens=1, do_sample=True, num_beams=1, top_p=0.95, temperature=0.85, repetition_penalty=1.2, pad_token_id=tokenizer.eos_token_id, use_cache=True, early_stopping=True)
     output = tokenizer.decode(output[0], skip_special_tokens=True)
     
     # Split the input text and generated text, and return only the generated text
@@ -58,29 +72,13 @@ def infer(inp, chat_history):
     generated_text = tokenizer.decode(generated_tokens[len(input_tokens):], skip_special_tokens=True)
     #remove white spaces and multiple tab
     generated_text = re.sub(r'[ \t]{2,}', ' ', generated_text)
-    chat_history.append(generated_text)
+    save_to_hist = "<sep>" + generated_text + "<sep>"
+    chat_history.append(save_to_hist)
     return generated_text
 
-#optim = AdamW(model.parameters(), lr=1e-3, weight_decay=0.0)
-optim = Adafactor(
-    model.parameters(),
-    lr=1e-3,
-    eps=(1e-30, 1e-3),
-    clip_threshold=1.0,
-    decay_rate=0.8,
-    beta1=None,
-    weight_decay=0.0,
-    relative_step=False,
-    scale_parameter=False,
-    warmup_init=False,
-)
-
-print(optim)
-
-load(model, optim)
 
 while True:
-    user = input("USER: ")
+    user = input("USER:")
     if user == ("-end-"):
         print("GPT: Have A Nice Day")
         break
