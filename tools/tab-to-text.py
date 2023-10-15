89b2ad7f3e9c493f18e7ae10629de6c4a2aa7cc4
VINUK <58259367+VINUK0@users.noreply.github.com>
2023-10-15 22:32:24 +0530

    Delete tools/tab-to-text.py

--- tools/tab-to-text.py
+++
@@ -1,7 +0,0 @@
-dataset = "./data.tsv"
-newdataset = "./data.txt"
-
-with open(dataset) as f, open(newdataset, "a+") as ff:
-    for line in f:
-        for msg in line.strip().split("\t"):
-            ff.write(msg + "\n")
