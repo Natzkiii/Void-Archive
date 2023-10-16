adc861229512926e67dec5f94380758c82fdfc79
Natzkiii <148138658+Natzkiii@users.noreply.github.com>
2023-10-16 20:39:04 +0200

    Create inst.bat
    added an automated install option

---
+++ inst.bat
@@ -0,0 +1,5 @@
+@echo off
+echo if you have python (3.10 to 3.8) you can continue here, as this is a script that will automate the installation of the required libraries to run.
+pause>NUL
+pip install requirements.txt
+exit
